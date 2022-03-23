14bc73c19ef16e096bc27159e384f0672eaae304
Trevor Bakker <tbakker@gmail.com>
2022-03-23 19:36:52 +0000

    Added unit test framework

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "mavalloc.h"
+#include "tinytest.h"
 #include <stdio.h>
 
 int test_case_1()
@@ -6,13 +7,11 @@ int test_case_1()
   char * ptr = ( char * ) mavalloc_alloc ( 65535 );
   mavalloc_free( ptr ); 
 
-  return 0;
+  return 1;
 }
 
 int test_case_2()
 {
-  printf("Running test 2 to test a block split and reuse\n");
-
   mavalloc_init( 2048, FIRST_FIT );
 
   char * ptr1 = ( char * ) mavalloc_alloc( 2048 );
@@ -25,20 +24,37 @@ int test_case_2()
 
   mavalloc_destroy( );
 
-  return 0;
+  return 1;
 }
 
-int main( int argc, char *argv[] )
+int test_case_3()
 {
+  // intentionally fail this test for now
+  
+  TINYTEST_EQUAL(1, 0);
 
-  printf("Running the unit tests\n");
- 
-  size_t bytes = 65535;
-
-  mavalloc_init( bytes, FIRST_FIT );
-
-
-  mavalloc_destroy( );
-
-  return 0;
+  return 1;
 }
+
+int tinytest_setup(const char *pName)
+{
+    fprintf( stderr, "tinytest_setup(%s)\n", pName);
+    return 0;
+}
+
+
+
+int tinytest_teardown(const char *pName)
+{
+    fprintf( stderr, "tinytest_teardown(%s)\n", pName);
+    return 0;
+}
+
+
+TINYTEST_START_SUITE(MavAllocTestSuite);
+  TINYTEST_ADD_TEST(test_case_1,tinytest_setup,tinytest_teardown);
+  TINYTEST_ADD_TEST(test_case_2,tinytest_setup,tinytest_teardown);
+  TINYTEST_ADD_TEST(test_case_3,tinytest_setup,tinytest_teardown);
+TINYTEST_END_SUITE();
+
+TINYTEST_MAIN_SINGLE_SUITE(MavAllocTestSuite);

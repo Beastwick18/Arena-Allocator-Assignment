6f28b4ce5aa92ffdf783486ef71f364bdef6315e
Trevor Bakker <tbakker@gmail.com>
2022-04-01 20:38:35 +0000

    Updated some unit tests

--- main.c
+++ main.c
@@ -4,36 +4,103 @@
 
 int test_case_1()
 {
+  mavalloc_init( 65535, BEST_FIT );
   char * ptr = ( char * ) mavalloc_alloc ( 65535 );
-  mavalloc_free( ptr ); 
 
-  return 1;
+  int size = mavalloc_size();
+
+  TINYTEST_EQUAL( size, 1); 
 }
 
 int test_case_2()
 {
-  mavalloc_init( 2048, FIRST_FIT );
+  mavalloc_init( 128000, BEST_FIT );
 
-  char * ptr1 = ( char * ) mavalloc_alloc( 2048 );
+  char * ptr1    = (char*)mavalloc_alloc( 65535 );
+  char * buffer1 = (char*)mavalloc_alloc( 1 );
+  char * ptr2    = (char*)mavalloc_alloc( 65 );
+  char * buffer2 = (char*)mavalloc_alloc( 1 );
+  char * ptr3    = (char*)mavalloc_alloc( 6000 );
 
   mavalloc_free( ptr1 );
+  mavalloc_free( ptr3 );
 
-  char * ptr2 = ( char * ) mavalloc_alloc( 1024 );
+  char * val = (char*) mavalloc_alloc( 1000 );
 
-  mavalloc_free( ptr2 );
-
-  mavalloc_destroy( );
-
-  return 1;
+  TINYTEST_EQUAL( val, ptr3); 
 }
 
 int test_case_3()
 {
-  // intentionally fail this test for now
-  
-  TINYTEST_EQUAL(1, 0);
+  mavalloc_init( 128000, WORST_FIT );
 
-  return 1;
+  char * ptr1    = (char*)mavalloc_alloc( 65535 );
+  char * buffer1 = (char*)mavalloc_alloc( 1 );
+  char * ptr2    = (char*)mavalloc_alloc( 65 );
+  char * buffer2 = (char*)mavalloc_alloc( 1 );
+  char * ptr3    = (char*)mavalloc_alloc( 6000 );
+
+  mavalloc_free( ptr1 );
+  mavalloc_free( ptr3 );
+
+  char * val = (char*) mavalloc_alloc( 1000 );
+
+  TINYTEST_EQUAL( val, ptr1); 
+}
+
+int test_case_4()
+{
+  mavalloc_init( 65535, FIRST_FIT );
+
+  char * ptr1  = ( char * ) mavalloc_alloc ( 10000 );
+  char * ptr2  = ( char * ) mavalloc_alloc ( 65 );
+
+  int size = mavalloc_size();
+
+  TINYTEST_EQUAL( size, 3); 
+}
+
+int test_case_5()
+{
+  mavalloc_init( 65535, NEXT_FIT );
+
+  char * ptr1  = ( char * ) mavalloc_alloc ( 1000 );
+  char * buf1  = ( char * ) mavalloc_alloc ( 1 );
+  char * ptr2  = ( char * ) mavalloc_alloc ( 10 );
+  char * buf2  = ( char * ) mavalloc_alloc ( 1 );
+  char * ptr3  = ( char * ) mavalloc_alloc ( 6000 );
+  char * buf3  = ( char * ) mavalloc_alloc ( 1 );
+  char * ptr4  = ( char * ) mavalloc_alloc ( 1000 );
+  char * buf4  = ( char * ) mavalloc_alloc ( 1 );
+
+  char * right_answer = ptr4;
+
+
+  ptr1 = ptr1;
+  ptr2 = ptr2;
+  ptr3 = ptr3;
+  ptr4 = ptr4;
+
+  buf1 = buf1;
+  buf2 = buf2;
+  buf3 = buf3;
+  buf4 = buf4;
+
+  mavalloc_free( ptr1 );
+  mavalloc_free( ptr2 );
+  mavalloc_free( ptr3 );
+  mavalloc_free( ptr4 );
+
+
+  char * ptr5 = ( char * ) mavalloc_alloc( 6000 );
+
+  ptr5 = ptr5;
+
+  char * ptr6 = ( char * ) mavalloc_alloc( 1000 );
+
+  ptr6 = ptr6;
+
+  TINYTEST_EQUAL( ptr6, right_answer ); 
 }
 
 int tinytest_setup(const char *pName)
@@ -53,8 +120,10 @@ int tinytest_teardown(const char *pName)
 
 TINYTEST_START_SUITE(MavAllocTestSuite);
   TINYTEST_ADD_TEST(test_case_1,tinytest_setup,tinytest_teardown);
-  TINYTEST_ADD_TEST(test_case_2,tinytest_setup,tinytest_teardown);
-  TINYTEST_ADD_TEST(test_case_3,tinytest_setup,tinytest_teardown);
+  //TINYTEST_ADD_TEST(test_case_2,tinytest_setup,tinytest_teardown);
+  //TINYTEST_ADD_TEST(test_case_3,tinytest_setup,tinytest_teardown);
+  TINYTEST_ADD_TEST(test_case_4,tinytest_setup,tinytest_teardown);
+  TINYTEST_ADD_TEST(test_case_5,tinytest_setup,tinytest_teardown);
 TINYTEST_END_SUITE();
 
 TINYTEST_MAIN_SINGLE_SUITE(MavAllocTestSuite);
